4c589d23f3089e5d7b1862527baf99395abe8b7b
Jonathan Winters <jonathan_winters@intuit.com>
2021-10-01 10:26:00 -0700

    css round 3

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,8 +1,8 @@
 export const AppHeader = ({headerText}) => {
     return (
         <header id="page-header">
-            <h1>{headerText}</h1>
             <img src={process.env.PUBLIC_URL + '/CatDog.jpg'} alt="" />
+            <h1>{headerText}</h1>
             <h2> .. Carpe Diem</h2>
         </header>
     )
